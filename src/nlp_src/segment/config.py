e32802f8b14a263b94eda8fa21690a7834674732
wangzhefeng <wangzhefengr@163.com>
2021-02-10 18:27:19 +0800

    update segment

---
+++ src/nlp_src/segment/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+import os
+
+
+root_dir = "/Users/zfwang/project/machinelearning/deeplearning/deeplearning/src/nlp_src"
+data_dir = os.path.join(root_dir, "data")
+model_dir = os.path.join(root_dir, "model")
+utils_dir = os.path.join(root_dir, "utils")

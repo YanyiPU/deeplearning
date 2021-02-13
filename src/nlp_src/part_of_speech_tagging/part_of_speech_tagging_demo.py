c6cc6bbea9f886fd9d4c28ce911e0a2de581d5bd
wangzhefeng <wangzhefengr@163.com>
2021-02-13 15:54:32 +0800

    test

--- src/nlp_src/part_of_speech_tagging/part_of_speech_tagging_demo.py
+++ src/nlp_src/part_of_speech_tagging/part_of_speech_tagging_demo.py
@@ -2,13 +2,21 @@
 import jieba.posseg as psg
 
 
-# data
-sent = "中文分词是文本处理不可或缺的一步!"
+def get_part_of_speech_taging(sentence, HMM = True):
+    """
+    词性标注
+    Params:
+        HMM=False: 非 HMM 词性标注
+        HMM=True: HMM 词性标注
+    """
+    segment_list = psg.cut(sentence, HMM)
+    tagged_sentence = " ".join([f"{w}/{t}" for w, t in segment_list])
+    
+    return tagged_sentence
 
-# 非 HMM 词性标注
-seg_list = psg.cut(sent)
-print(" ".join([f"{w}/{t}" for w, t in seg_list]))
 
-# HMM 词性标注
-seg_list_hmm = psg.cut(sent, HMM = True)
-print(" ".join([f"{w}/{t}" for w, t in seg_list_hmm]))
+if __name__ == "__main__":
+    # data
+    sentence = "中文分词是文本处理不可或缺的一步!"
+    tagged_sentence = get_part_of_speech_taging(sentence)
+    print(tagged_sentence)

1449e8d6a38a2ef1913e182e55a411caceff67db
wangzhefeng <wangzhefengr@163.com>
2021-08-24 22:04:40 +0800

    test

--- src/project_image/image_classification/leaf_classification/config.py
+++ src/project_image/image_classification/leaf_classification/config.py
@@ -2,6 +2,6 @@
 import os
 
 
-root_dir = "/Users/zfwang/project/machinelearning"
+root_dir = "/Users/zfwang/machinelearning"
 project_dir = os.path.join(root_dir, "deeplearning/src/project_image/image_classification/leaf_classification")
 data_dir = os.path.join(root_dir, "datasets/leaf_classification")
